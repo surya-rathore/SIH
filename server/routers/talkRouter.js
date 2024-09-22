5eb74874ee7da88e419087a05f267037ffcee9ef
Suraj Bali Sahu <sahusurajbali@gmail.com>
2024-09-22 22:12:29 +0530

    Delete server directory

--- server/routers/talkRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const talkrouter = express.Router();
-const talkController = require('../controllers/talkController');
-
-talkrouter.post('/talkwithAi', talkController.talkwithAi);
-
-module.exports = talkrouter;
